7e52c467853899d045564b6bab5cb8dc0f06c347
MOHAMMED UMAR FAAZITH K <123172380+MOHAMMEDUMARFAAZITH@users.noreply.github.com>
2023-05-02 22:43:25 +0530

    Update sql.py

--- sql.py
+++ sql.py
@@ -1,9 +1,7 @@
 import streamlit as st
 import pandas as pd
 import plotly.express as px
-import sqlalchemy
 import pymysql
-import pandas as pd
 import sqlalchemy
 from sqlalchemy import text
 import socket
